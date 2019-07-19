4cc100145602d09cf4e9642f9be1c32412a715be
Derek Stephen McLean <u1168896@marsh.com>
2019-07-19 16:49:46 +0100

    view files

--- src/GeneratorService.php
+++ src/GeneratorService.php
@@ -61,6 +61,7 @@ class GeneratorService
             'build/' . $unique . '/' . $entityName . '/Repository',
             'build/' . $unique . '/' . $entityName . '/Service',
             'build/' . $unique . '/' . $entityName . '/View',
+            'build/' . $unique . '/' . $entityName . '/View/' . $entityName,
         ];
 
         foreach ($folders as $folder) {
@@ -71,7 +72,7 @@ class GeneratorService
 
         if (!file_exists('migrations')) {
             if (!mkdir('migrations') && !is_dir('migrations')) {
-                throw new Exception('could not create ' . $folder);
+                throw new Exception('could not create migrations folder.');
             }
         }
 
@@ -243,6 +244,78 @@ return new JsonResponse([\'deleted\' => true]);');
      */
     private function createView(string $nameSpace, string $entityName, array $fields): bool
     {
+        $name = strtolower($entityName);
+
+        // create
+        $code = '<a href="/' . $name . '" class="btn btn-default pull-right">Back</a>
+<h1>Add a ' . $entityName . '</h1>
+<?= $msg . $form;' . "\n";
+        file_put_contents('build/' . $this->buildId . '/'. $entityName . '/View/'. $entityName . '/create.php', $code);
+
+        // delete
+        $code = '<a href="/' . $name . '" class="btn btn-default pull-right">Back</a>
+<h1>Delete ' . $entityName . '</h1>
+<?= $msg . $form;' . "\n";
+        file_put_contents('build/' . $this->buildId . '/'. $entityName . '/View/'. $entityName . '/delete.php', $code);
+
+        // edit
+        $code = '<a href="/' . $name . '" class="btn btn-default pull-right">Back</a>
+<h1>Edit ' . $entityName . '</h1>
+<?= $msg . $form;' . "\n";
+        file_put_contents('build/' . $this->buildId . '/'. $entityName . '/View/'. $entityName . '/edit.php', $code);
+
+        // index
+        $code = '<?php
+use Del\Icon;
+?>
+<a href="/' . $name . '/create" class="btn btn-default pull-right"><?= Icon::ADD ;?> Add a '. $entityName . '</a>
+<h1>'. $entityName . ' Admin</h1>
+<?= $paginator ?>
+<table class="table table-condensed table-bordered">
+    <thead>
+        <tr>
+            <td>Id</td>
+            <td>Name</td>
+            <td>Edit</td>
+            <td>Delete</td>
+        </tr>
+    </thead>
+    <tbody>
+    <?php
+    /** @var \BoneMvc\Module\\'. $entityName . '\Entity\\'. $entityName . ' $' . $name . ' */
+    foreach ($' . $name . 's as $' . $name . ') { ?>
+        <tr>
+            <td><a href="/' . $name . '/<?= $' . $name . '->getId() ?>"><?= $' . $name . '->getId() ;?></a></td>
+            <td><?= $' . $name . '->getName() ;?></td>
+            <td><a href="/' . $name . '/edit/<?= $' . $name . '->getId() ?>"><?= Icon::EDIT ;?></a></td>
+            <td><a href="/' . $name . '/delete/<?= $' . $name . '->getId() ?>"><?= Icon::REMOVE ;?></a></td>
+        </tr>
+    <?php } ?>
+    </tbody>
+
+</table>' . "\n";
+        file_put_contents('build/' . $this->buildId . '/'. $entityName . '/View/'. $entityName . '/index.php', $code);
+
+
+        // view
+        $code = '<?php
+use Del\Icon;
+
+/** @var \BoneMvc\Module\\'. $entityName . '\Entity\\'. $entityName . ' $'. $name . ' */
+?>
+<a href="/'. $name . '" class="btn btn-default pull-right"><?= Icon::CARET_LEFT ;?> Back</a>
+
+<h1>View '. $entityName . '</h1>
+<div class="well">
+    <h2><?= $'. $name . '->getName() ?></h2>
+</div>
+<a href="/'. $name . '/edit/<?= $'. $name . '->getId() ?>" class="btn btn-default">
+    <?= Icon::EDIT ;?> Edit
+</a>' . "\n";
+        file_put_contents('build/' . $this->buildId . '/'. $entityName . '/View/'. $entityName . '/view.php', $code);
+
+        /** @todo ask which fields need displayed and searchable */
+
         return true;
     }
 
