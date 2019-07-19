efe3462a6ae9273c4f111e0c081c763bf1ed6810
Derek Stephen McLean <u1168896@marsh.com>
2019-07-19 16:30:37 +0100

    Api Controller

--- src/GeneratorService.php
+++ src/GeneratorService.php
@@ -37,6 +37,7 @@ class GeneratorService
         $this->createForm($nameSpace, $entityName, $fields);
         $this->createApiController($nameSpace, $entityName, $fields);
         $this->createController($nameSpace, $entityName, $fields);
+        $this->createView($nameSpace, $entityName, $fields);
         $this->createPackage($nameSpace, $entityName);
 
         return $this->buildId;
@@ -86,6 +87,161 @@ class GeneratorService
      * @return bool
      */
     private function createApiController(string $nameSpace, string $entityName, array $fields): bool
+    {
+        $file = new PhpFile();
+        $file->setStrictTypes();
+        $moduleNamespace = $nameSpace . '\\' . $entityName;
+        $namespace = $file->addNamespace($moduleNamespace . '\\Controller');
+        $name = lcfirst($entityName);
+
+        $namespace->addUse($moduleNamespace . '\\Collection\\' . $entityName . 'Collection');
+        $namespace->addUse($moduleNamespace . '\\Form\\' . $entityName . 'Form');
+        $namespace->addUse($moduleNamespace . '\\Service\\' . $entityName . 'Service');
+        $namespace->addUse('League\Route\Http\Exception\NotFoundException');
+        $namespace->addUse(ResponseInterface::class);
+        $namespace->addUse(ServerRequestInterface::class);
+        $namespace->addUse('Zend\Diactoros\Response\JsonResponse');
+
+        $class = $namespace->addClass('ApiController');
+
+        $property = $class->addProperty('service');
+        $property->addComment('@param ' . $entityName . 'Service $service');
+        $property->setVisibility('private');
+
+        // index action
+        $method = $class->addMethod('indexAction');
+        $method->addComment('@param ServerRequestInterface $request');
+        $method->addComment('@param array $args');
+        $method->addComment('@return ResponseInterface');
+        $method->addComment('@throws NotFoundException');
+        $method->addComment('@throws \Doctrine\ORM\NonUniqueResultException');
+        $method->addParameter('request')->setTypeHint(ServerRequestInterface::class);
+        $method->addParameter('args')->setTypeHint('array');
+        $method->setReturnType(ResponseInterface::class);
+        $method->setBody('$params = $request->getQueryParams();
+$limit = $params[\'limit\'];
+$offset = $params[\'offset\'];
+$db = $this->service->getRepository();
+$' . $name . 's = new ' . $entityName . 'Collection($db->findBy([], null, $limit, $offset));
+$total = $db->getTotal' . $entityName . 'Count();
+$count = count($' . $name . 's);
+if ($count < 1) {
+    throw new NotFoundException();
+}
+
+$payload[\'_embedded\'] = $' . $name . 's->toArray();
+$payload[\'count\'] = $count;
+$payload[\'total\'] = $total;
+
+return new JsonResponse($payload);');
+
+
+        // create action
+        $method = $class->addMethod('createAction');
+        $method->addComment('@param ServerRequestInterface $request');
+        $method->addComment('@param array $args');
+        $method->addComment('@return ResponseInterface');
+        $method->addComment('@throws \Doctrine\ORM\ORMException');
+        $method->addComment('@throws \Doctrine\ORM\OptimisticLockException');
+        $method->addParameter('request')->setTypeHint(ServerRequestInterface::class);
+        $method->addParameter('args')->setTypeHint('array');
+        $method->setReturnType(ResponseInterface::class);
+        $method->setBody('$post = json_decode($request->getBody()->getContents(), true) ?: $request->getParsedBody();
+$form = new ' . $entityName . 'Form(\'create\');
+$form->populate($post);
+
+if ($form->isValid()) {
+    $data = $form->getValues();
+    $' . $name . ' = $this->service->createFromArray($data);
+    $this->service->save' . $entityName . '($' . $name . ');
+
+    return new JsonResponse($' . $name . '->toArray());
+}
+
+return new JsonResponse([
+    \'error\' => $form->getErrorMessages(),
+]);');
+
+
+        // view action
+        $method = $class->addMethod('viewAction');
+        $method->addComment('@param ServerRequestInterface $request');
+        $method->addComment('@param array $args');
+        $method->addComment('@return ResponseInterface');
+        $method->addComment('@throws \Doctrine\ORM\EntityNotFoundException');
+        $method->addParameter('request')->setTypeHint(ServerRequestInterface::class);
+        $method->addParameter('args')->setTypeHint('array');
+        $method->setReturnType(ResponseInterface::class);
+        $method->setBody('$' . $name . ' = $this->service->getRepository()->find($args[\'id\']);
+
+return new JsonResponse($' . $name . '->toArray());');
+
+
+        // update action
+        $method = $class->addMethod('updateAction');
+        $method->addComment('@param ServerRequestInterface $request');
+        $method->addComment('@param array $args');
+        $method->addComment('@return ResponseInterface');
+        $method->addComment('@throws \Doctrine\ORM\EntityNotFoundException');
+        $method->addComment('@throws \Doctrine\ORM\ORMException');
+        $method->addComment('@throws \Doctrine\ORM\OptimisticLockException');
+        $method->addParameter('request')->setTypeHint(ServerRequestInterface::class);
+        $method->addParameter('args')->setTypeHint('array');
+        $method->setReturnType(ResponseInterface::class);
+        $method->setBody('$db = $this->service->getRepository();
+$' . $name . ' = $db->find($args[\'id\']);
+
+$post = json_decode($request->getBody()->getContents(), true) ?: $request->getParsedBody();
+$form = new ' . $entityName . 'Form(\'update\');
+$form->populate($post);
+
+if ($form->isValid()) {
+    $data = $form->getValues();
+    $' . $name . ' = $this->service->updateFromArray($' . $name . ', $data);
+    $this->service->save' . $entityName . '($' . $name . ');
+
+    return new JsonResponse($' . $name . '->toArray());
+}
+
+return new JsonResponse([
+    \'error\' => $form->getErrorMessages(),
+]);');
+
+
+        // delete action
+        $method = $class->addMethod('updateAction');
+        $method->addComment('@param ServerRequestInterface $request');
+        $method->addComment('@param array $args');
+        $method->addComment('@return ResponseInterface');
+        $method->addComment('@throws \Doctrine\ORM\EntityNotFoundException');
+        $method->addComment('@throws \Doctrine\ORM\ORMException');
+        $method->addComment('@throws \Doctrine\ORM\OptimisticLockException');
+        $method->addParameter('request')->setTypeHint(ServerRequestInterface::class);
+        $method->addParameter('args')->setTypeHint('array');
+        $method->setReturnType(ResponseInterface::class);
+        $method->setBody('$db = $this->service->getRepository();
+$' . $name . ' = $db->find($args[\'id\']);
+$this->service->delete' . $entityName . '($' . $name . ');
+
+return new JsonResponse([\'deleted\' => true]);');
+
+        $printer = new PsrPrinter();
+        $code = $printer->printFile($file);
+        file_put_contents('build/' . $this->buildId . '/'. $entityName . '/Controller/ApiController.php', $code);
+
+
+        return true;
+    }
+
+
+
+    /**
+     * @param string $nameSpace
+     * @param string $entityName
+     * @param array $fields
+     * @return bool
+     */
+    private function createView(string $nameSpace, string $entityName, array $fields): bool
     {
         return true;
     }
@@ -711,10 +867,7 @@ $c[\'service.' . $entityName . '\'] = new ' . $entityName . 'Service($em);');
         $namespace->addUse(ServerRequestInterface::class);
         $namespace->addUse('Zend\Diactoros\Response\HtmlResponse');
 
-        $class = new ClassType($entityName . 'Controller');
-        $name = lcfirst($entityName);
-
-        $namespace->add($class);
+        $class = $namespace->addClass($entityName . 'Controller');
 
         $property = $class->addProperty('numPerPage');
         $property->addComment('@var int $numPerPage');
